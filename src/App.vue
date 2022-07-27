4620e9189e4f1bceb2ca81e40c18f405f0395250
liuxiang <liuxiang@dachkj.com>
2022-07-27 10:05:30 +0800

    case

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-main>
+      <v-sheet>
+        <v-subheader>title</v-subheader>
+        <v-divider />
+        <v-list max-height="400" class="overflow-auto">
+          <v-list-item-group>
+            <template v-for="(org, index) in list">
+              <v-list-item :key="org.cid" >
+               {{org.cid}}
+              </v-list-item>
+              <v-divider :key="index"></v-divider>
+            </template>
+          </v-list-item-group>
+        </v-list>
+      </v-sheet>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
